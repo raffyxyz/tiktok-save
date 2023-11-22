c97aa083635ffe3019078464151fc0572d451bbb
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-11-22 17:34:06 +0800

    added history list

---
+++ lib/models/History.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'History.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class HistoryAdapter extends TypeAdapter<History> {
+  @override
+  final int typeId = 1;
+
+  @override
+  History read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return History(
+      cover: fields[0] as String,
+      author: fields[1] as String,
+      title: fields[3] as String,
+      filePath: fields[4] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, History obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.cover)
+      ..writeByte(1)
+      ..write(obj.author)
+      ..writeByte(3)
+      ..write(obj.title)
+      ..writeByte(4)
+      ..write(obj.filePath);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HistoryAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
