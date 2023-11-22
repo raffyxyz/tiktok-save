efcf0e2a63527cabbf22aa2bf3c93448eac258b3
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-11-22 17:34:06 +0800

    added history list

---
+++ lib/models/History.dart
@@ -0,0 +1,22 @@
+// ignore: file_names
+import 'package:hive/hive.dart';
+part 'History.g.dart';
+
+@HiveType(typeId: 1)
+class History {
+  @HiveField(0)
+  final String cover;
+  @HiveField(1)
+  final String author;
+  @HiveField(3)
+  final String title;
+  @HiveField(4)
+  final String filePath;
+
+  History({
+    required this.cover,
+    required this.author,
+    required this.title,
+    required this.filePath,
+  });
+}
---
+++ lib/widgets/history/history_widget.dart
@@ -0,0 +1,54 @@
+import 'package:downloader_app/widgets/card.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+
+class HistoryWidget extends StatefulWidget {
+  const HistoryWidget({super.key});
+
+  @override
+  State<HistoryWidget> createState() => _HistoryWidgetState();
+}
+
+class _HistoryWidgetState extends State<HistoryWidget> {
+  late final Box historyBox;
+
+  @override
+  void initState() {
+    super.initState();
+    historyBox = Hive.box('historyBox');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Expanded(
+        child: ValueListenableBuilder(
+          valueListenable: historyBox.listenable(),
+          builder: (context, Box box, widget) {
+            if (box.isEmpty) {
+              return const Text("Empty");
+            } else {
+              return ListView.builder(
+                itemCount: box.length,
+                itemBuilder: (context, index) {
+                  var currentBox = box;
+                  var historyData = currentBox.getAt(box.length - 1 - index)!;
+
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: MyCard(
+                      cover: historyData.cover,
+                      author: historyData.author,
+                      title: historyData.title,
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
