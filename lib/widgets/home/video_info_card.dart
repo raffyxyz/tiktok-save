0fa3c126f9d8ba71e7be286c42ceba5beffc1902
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-11-23 12:04:36 +0800

    new widgets

---
+++ lib/widgets/home/video_info_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class VideoInfoCard extends StatelessWidget {
+  final String cover;
+  final String author;
+  final String title;
+  final List<Widget> children;
+
+  const VideoInfoCard({
+    super.key,
+    required this.cover,
+    required this.author,
+    required this.title,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Container(
+              color: Colors.grey.shade100,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(5.0),
+                          child: SizedBox(
+                            width: 80,
+                            height: 80,
+                            child: FittedBox(
+                              fit: BoxFit.cover,
+                              child: Image.network(cover),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                author,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                title.length > 65
+                                    ? title.substring(0, 65)
+                                    : title,
+                                softWrap: true,
+                                style: const TextStyle(
+                                  fontSize: 12.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    ...children,
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
