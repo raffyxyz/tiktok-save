9158cb91612f2a97c8d4cf49409f9680c9345ae1
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-11-26 14:01:38 +0800

    update: added connection logic

--- lib/widgets/home/home_widget.dart
+++ lib/widgets/home/home_widget.dart
@@ -1,9 +1,16 @@
+import 'dart:async';
+import 'dart:developer' as developer;
+
 import 'package:downloader_app/api/tiktok_api.dart';
 import 'package:downloader_app/models/Tiktok.dart';
 import 'package:downloader_app/widgets/home/guide_widget.dart';
 import 'package:downloader_app/widgets/home/result.dart';
-import 'package:flutter/material.dart';
 import 'package:downloader_app/widgets/text_field.dart';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/services.dart';
+
+import 'package:flutter/material.dart';
 
 class HomeWidget extends StatefulWidget {
   const HomeWidget({super.key});
@@ -13,6 +20,12 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  // Network connection.
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  // Tiktok api request.
   final tiktokLinkController = TextEditingController();
   bool isValidUrl = false;
   late final TiktokApiClient tiktokApiClient;
@@ -24,12 +37,50 @@ class _HomeWidgetState extends State<HomeWidget> {
 
     tiktokApiClient = TiktokApiClient(apiUrl: '');
     tiktokInfo = tiktokApiClient.fetchTiktokInfo();
+
+    initConnectivity();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
   }
 
   @override
   void dispose() {
     tiktokLinkController.dispose();
+    _connectivitySubscription.cancel();
     super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
+  }
+
+  bool _isConnectionPresent() {
+    return _connectionStatus.toString() == "ConnectivityResult.wifi" ||
+        _connectionStatus.toString() == "ConnectivityResult.mobile";
   }
 
   bool _isValidTiktokLink(String url) {
@@ -61,6 +112,7 @@ class _HomeWidgetState extends State<HomeWidget> {
             obscureText: false,
             icon: const Icon(Icons.link_outlined),
             borderRadius: 5.0,
+            readOnly: _isConnectionPresent() ? false : true,
             onChange: (value) => _handleLinkChange(value),
           ),
           const SizedBox(
@@ -89,7 +141,11 @@ class _HomeWidgetState extends State<HomeWidget> {
                         id: "${snapshot.data!.data?.id}",
                       );
               } else {
-                return const GuideWidget();
+                return _isConnectionPresent()
+                    ? const GuideWidget()
+                    : const Center(
+                        child: Text("No network."),
+                      );
               }
             },
           ),
