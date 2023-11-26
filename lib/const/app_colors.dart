641b22f1d230584a8795c98db319916e08d01e23
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-11-26 13:32:23 +0800

    added: color constants

---
+++ lib/const/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Background Colors.
+  static const Color backgroundColor = Colors.white;
+  static Color secondaryBgColor = Colors.grey.shade100;
+  static Color primaryColor = Colors.purple;
+
+  // Text Colors.
+  static Color secondaryTextColor = Colors.black45;
+  static Color foreGroundColor = Colors.grey.shade800;
+
+  // Error Colors
+  static Color errorColor = Colors.red;
+}
