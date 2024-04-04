7025c15f316787e7dc2ffdc33e37e8906286d39b
raffyxyz <raffy.amoguis1@gmail.com>
2024-04-04 10:17:41 +0800

    chore: cleanup

--- lib/widgets/home/video_info_card.dart
+++ lib/widgets/home/video_info_card.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:downloader_app/const/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class VideoInfoCard extends StatelessWidget {
